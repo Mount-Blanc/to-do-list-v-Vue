fc008cd74da6b957233e16985a04824ad1814b1e
Mount-Blanc <mountblanc87@gmail.com>
2022-07-04 10:42:46 -0700

    created a watch function to change value of name to new value

--- To-do/src/App.vue
+++ To-do/src/App.vue
@@ -11,6 +11,15 @@ const todos_asc = computed ( () => todos.value.sort((a,b) => {
   return b.createdAt - a.createdAt
 }))
 
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal)
+})
+
+onMounted (() => {
+  name.value = localStorage.getItem('name') || ''
+})
+
 </script>
 
 <template>
