b4707124af02c3c3dd54a6bf755d26ea84d2e58c
Mount-Blanc <mountblanc87@gmail.com>
2022-07-04 10:36:32 -0700

    created variables to hold basic data

--- To-do/src/App.vue
+++ To-do/src/App.vue
@@ -1,20 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos = ref( [])
+const name = ref('')
+
+const input_content =ref('')
+const input_category = ref(null)
+
+const todos_asc = computed ( () => todos.value.sort((a,b) => {
+  return b.createdAt - a.createdAt
+}))
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  
 </template>
 
 <style>
